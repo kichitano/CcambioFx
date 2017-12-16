630f6c8e7f565414b7f163a33c552b7b1100744e
kichitano <30935429+kichitano@users.noreply.github.com>
2017-12-16 13:23:09 -0500

    CCasaCambio v0.02

--- CCambioFx/src/PkgAplicacion/Menu.java
+++ CCambioFx/src/PkgAplicacion/Menu.java
@@ -1,10 +1,11 @@
-    /*
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package PkgAplicacion;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -13,18 +14,18 @@ import javafx.stage.Stage;
 
 /**
  *
- * @author Gary
+ * @author Corei7
  */
-public class Login extends Application {
+public class Menu extends Application {
     
     @Override
-    public void start(Stage loginStage) throws Exception {
+    public void start(Stage menuStage) throws IOException {
         
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("LoginDesign.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MenuDesign.fxml"));
         Parent root = loader.load();
         Scene scene = new Scene(root);
-        loginStage.setScene(scene);
-        loginStage.show();
+        menuStage.setScene(scene);
+        menuStage.show();
         
     }
 
